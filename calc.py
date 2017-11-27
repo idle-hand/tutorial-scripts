e1231dca7419e12be1e8bf1518c7403cdd9bb048
DAVID HOWE <33843929+idle-hand@users.noreply.github.com>
2017-11-27 03:54:42 -0500

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,36 @@
+class Calc:
+    
+    def add(a,b):
+        add = a + b
+        print(add)
+        
+    def sub(a,b):
+        sub = a - b
+        print(sub)
+
+    def mult(a,b):
+        mult = a * b
+        print(mult)
+
+    def div(a,b):
+        div = a / b
+        print(div)
+
+
+Calc.div(25,5)
+
+Calc.add(37,66)
+
+Calc.sub(43,13)
+
+Calc.mult(42,2)
+
+
+
+
+
+
+
+
+
+        
